a5bcf5138d73b505530fe9160ec1f83514d962b9
git practice <nivi@abc.com>
2023-03-04 16:33:13 -0500

    files updated

--- my-java-app/entrypoint.sh
+++ my-java-app/entrypoint.sh
@@ -4,9 +4,9 @@ echo "TEST_CLASS running: $TEST_CLASS"
 echo "EXTRA_ENVS: $EXTRA_ENVS"
 
 if [[ $TEST_CLASS == "All" ]]; then
-    java -jar sre-hello-world-entire-tests.jar
+    java -jar my-java-app-test.jar
 else
-    java -jar -DextraEnvs=$EXTRA_ENVS sre-hello-world-entire-tests.jar -s $TEST_CLASS
+    java -jar -DextraEnvs=$EXTRA_ENVS my-java-app-test.jar -s $TEST_CLASS
 fi
 
 # upload report files
